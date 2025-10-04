b02d0f53ada0c7f2badafa44560a57da128eda2e
Pankaj Mishra <195760573+ParulUniversityMishraPankajKumar@users.noreply.github.com>
2025-10-04 16:45:15 +0530

    new update

--- js/custom.js
+++ js/custom.js
@@ -53,7 +53,7 @@ $(function () {
             logo = $(".navbar .logo> img");
         if (bodyScroll > 100) {
             navbar.addClass("nav-scroll");
-            logo.attr('src', '../img/logo.png');
+            logo.attr('src', '../img/pankaj/logo.png');
         } else {
             navbar.removeClass("nav-scroll");
             logo.attr('src', '../img/pankaj/white-logo.svg');
